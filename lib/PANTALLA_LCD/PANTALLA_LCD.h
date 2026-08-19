3da8122667c6c86051b0bc7f9ec61945c46dbcb6
orellana y gonzales <tizisantos.9@gmail.com>
2026-08-19 14:10:43 -0300

    Refactor LCD h

--- lib/PANTALLA_LCD/PANTALLA_LCD.h
+++ lib/PANTALLA_LCD/PANTALLA_LCD.h
@@ -1,6 +1,27 @@
-#ifndef PANTALLA_LCD_h
-#define PANTALLA_LCD_h
-#include "stm32f103xb.h"
-Void lcd_init(int columnas,int filas);
-char lcd (int num,int datos);
+#ifndef PANTALLA_LCD_H
+#define PANTALLA_LCD_H
+#include"stm32f103xb.h"
+
+typedef struct{
+   GPIO_TypeDef* puerto;
+   int datos[8];
+   int rs;
+   int rw;
+   int vo;
+   int cursor;
+   int col;
+   int fil;
+
+}LCD_t;
+
+void lcd_init(LCD_t*);
+void lcd_clear(LCD_t*);
+void lcd_setCursor(LCD_t* );
+void lcd_print();
+void lcd_scrollDisplayLeft(LCD_t* );
+void lcd_scrollDisplayRight(LCD_t* );
+void lcd_backlight();
+void lcd_noBacklight();
+void lcd_createChar (int num, int datos);
+
 #endif
